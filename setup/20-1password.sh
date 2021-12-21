ffcd67a3bbce0211bc5960143808b092856479b0
Markus Wolf <mail@markus-wolf.de>
2021-12-21 23:26:17 +0100

    fix: unzip cli options

--- setup/20-1password.sh
+++ setup/20-1password.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /tmp/1password.zip ] ; then
     curl -s -o /tmp/1password.zip https://cache.agilebits.com/dist/1P/op/pkg/v1.12.3/op_linux_amd64_v1.12.3.zip
-    unzip /tmp/1password.zip -o -d /tmp/
+    unzip -o /tmp/1password.zip -d /tmp/
 
     gpg --keyserver hkp://keyserver.ubuntu.com:80 --receive-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
     gpg --verify /tmp/op.sig /tmp/op
