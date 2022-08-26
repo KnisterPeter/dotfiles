40df6d2c15e2fa7714f4d6403de679acc738ef07
Markus Wolf <mail@markus-wolf.de>
2022-08-26 12:37:58 +0200

    feat: add fzf headers

--- functions/git.sh
+++ functions/git.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+__select_branch() {
+  header="$1"
+
+  git branch -a --color |sed -e 's/^\*\?\s*//g' |cut -d " " -f 1 |sed -e 's|remotes/||' |grep -v "HEAD" |fzf --no-sort --ansi --header "$header"
+}
+
 ___select_commit_from_current_branch() {
-  git ls --color |fzf --layout=reverse-list --no-sort --ansi |sed -e 's/^[| *]\+//' |cut -d ' ' -f 1
+  header="$1"
+
+  git ls --color |fzf --layout=reverse-list --no-sort --ansi --header "$header" |sed -e 's/^[| *]\+//' |cut -d ' ' -f 1
 }
 
 ___select_commit_from_all_branches() {
@@ -9,7 +17,10 @@ ___select_commit_from_all_branches() {
 }
 
 ___select_commit_from_branch() {
-  git ls "$1" --color |fzf --layout=reverse-list --no-sort --ansi |sed -e 's/^[| *]\+//' |cut -d ' ' -f 1
+  branch="$1"
+  header="$2"
+
+  git ls "$branch" --color |fzf --layout=reverse-list --no-sort --ansi --header "$header" |sed -e 's/^[| *]\+//' |cut -d ' ' -f 1
 }
 
 __git_show_commit() {
@@ -62,14 +73,21 @@ __git_rebase_interactive() {
 }
 
 __git_rebase_change_parent() {
-  target_branch="${1:-origin/master}"
+  target_branch="$1"
 
-  child_sha="$(___select_commit_from_current_branch)"
+  child_sha="$(___select_commit_from_current_branch "Commit to rebase")"
   if [ -z "$child_sha" ] ; then
     exit
   fi
 
-  new_parent_sha="$(___select_commit_from_branch "$target_branch")"
+  if [ -z "$target_branch" ] ; then
+    target_branch="$(__select_branch "Branch to rebase onto")"
+    if [ -z "$target_branch" ] ; then
+      exit
+    fi
+  fi
+
+  new_parent_sha="$(___select_commit_from_branch "$target_branch" "Commit to rebase onto")"
   if [ -z "$new_parent_sha" ] ; then
     exit
   fi
