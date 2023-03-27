8084c8264c72522e33a99d35c933666e022c07fe
Markus Wolf <mail@markus-wolf.de>
2023-03-27 11:08:45 +0200

    fix: setup deno in each terminal

--- setup/13-deno.sh
+++ setup/13-deno.sh
@@ -4,6 +4,3 @@ if ! which deno 2> /dev/null ; then
     echo "Deno not found. Installing..."
     curl -fsSL https://deno.land/x/install/install.sh | sh
 fi
-
-export DENO_INSTALL="$HOME/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
---
+++ tools/13-deno.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DENO_INSTALL="$HOME/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+if which deno > /dev/null ; then
+    # shellcheck disable=SC1090
+    source <(deno completions bash)
+fi
