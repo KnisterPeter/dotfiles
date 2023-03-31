c8d6310f47f7b93eb46fe41787e7df65f03721c5
Markus Wolf <mail@markus-wolf.de>
2023-03-31 18:31:44 +0200

    feat: enable 1password ssh-agent for linux

--- tools/05-ssh.sh
+++ tools/05-ssh.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -z "$INSIDE_WSL" ]; then
-    if [ -z "$SSH_AGENT_PID" ]; then
-        eval "$(ssh-agent)" > /dev/null
+    if [ -S "$HOME/.1password/agent.sock" ] ;then
+        export SSH_AUTH_SOCK=~/.1password/agent.sock
+    else
+        if [ -z "$SSH_AGENT_PID" ]; then
+            eval "$(ssh-agent)" > /dev/null
+        fi
     fi
 fi
