f6b2b821aff84b6718218913afb54900e6c6defc
Markus Wolf <mail@markus-wolf.de>
2023-11-16 13:10:57 +0100

    feat: add tabtab

---
+++ tools/16-tabtab.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# tabtab source for packages
+# uninstall by removing these lines
+if [ -f ~/.config/tabtab/bash/__tabtab.bash ] ; then
+  # shellcheck disable=SC1090
+  source ~/.config/tabtab/bash/__tabtab.bash
+fi
