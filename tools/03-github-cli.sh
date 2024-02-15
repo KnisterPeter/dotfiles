5433b7e7119dfc8fc1ca31d690d863c3e15e9456
Markus Wolf <mail@markus-wolf.de>
2024-02-15 13:07:53 +0000

    feat: add github cli aliases

--- tools/03-github-cli.sh
+++ tools/03-github-cli.sh
@@ -4,4 +4,11 @@ echo -n "  🔹 GitHub CLI"
 
 eval "$(gh completion -s bash)"
 
+if ! gh alias list | grep "create-draft:" > /dev/null ; then
+    gh alias set create-draft "pr create --assignee @me --fill-first --draft"
+fi
+if ! gh alias list | grep "ssh:" > /dev/null ; then
+    gh alias set ssh "codespace ssh -- -A -X"
+fi
+
 echo ""
