009c8d077e792109a26834c66fbe217a0dc83f11
Markus Wolf <mail@markus-wolf.de>
2024-11-06 11:25:02 +0100

    fix: update deno installation

--- setup/13-deno.sh
+++ setup/13-deno.sh
@@ -4,7 +4,7 @@ echo -n "Install Deno ... "
 
 if ! which deno > /dev/null 2>&1 ; then
     echo "Deno not found. Installing..."
-    curl -fsSL https://deno.land/x/install/install.sh | sh
+    curl -fsSL https://deno.land/install.sh | sh
 fi
 
 echo "✔️"
