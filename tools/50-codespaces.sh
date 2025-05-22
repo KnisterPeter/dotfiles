f15d01d0960579af5ef6d479776329dec00f9ce4
Markus Wolf <mail@markus-wolf.de>
2025-05-22 10:08:37 +0200

    refactor: update git config for codespaces

--- tools/50-codespaces.sh
+++ tools/50-codespaces.sh
@@ -5,14 +5,8 @@
 #
 
 if [ "$GITHUB_USER" = "markus-wolf_statista" ] ; then
-
-    cat <<EOF > "$HOME/.gitconfig.local"
-[user]
-	name = Markus Wolf
-	email = markus.wolf@statista.com
-	signingkey = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgEYt9y0j59T7INYfZhjiox+oAg0MPQ0+e57uBKmb1H
-[commit]
-	gpgSign = false
-EOF
-
+	git config set --global user.name "Markus Wolf"
+	git config set --global user.email "markus.wolf@statista.com"
+	git config set --global user.signingkey "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMgEYt9y0j59T7INYfZhjiox+oAg0MPQ0+e57uBKmb1H"
+	git config set --global commit.gpgsign false
 fi
