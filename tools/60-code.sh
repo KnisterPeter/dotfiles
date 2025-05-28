9fa5db9b4e36861092bacb82c8a13a87249dee8c
Markus Wolf <mail@markus-wolf.de>
2025-05-28 08:59:00 +0200

    feat: auto install vscode extensions

---
+++ tools/60-code.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+echo -n "  🔹 vscode"
+printed=0
+
+if which code > /dev/null ; then
+    expected=(
+        "github.codespaces"
+        "github.vscode-pull-request-github"
+        "streetsidesoftware.code-spell-checker"
+        "editorconfig.editorconfig"
+        "dbaeumer.vscode-eslint"
+        "github.vscode-github-actions"
+        "esbenp.prettier-vscode"
+        "gruntfuggly.todo-tree"
+        "ms-azuretools.vscode-containers"
+        "denoland.vscode-deno"
+    )
+    installed="$(code --list-extensions)"
+
+    for ext in "${expected[@]}"; do
+        # literal match is expected
+        # shellcheck disable=SC2076
+        if [[ ! "$installed" =~ "$ext" ]]; then
+            if [[ $printed -eq 0 ]]; then
+                echo " installing extensions:"
+                printed=1
+            fi
+
+            echo "      - $ext"
+            code --install-extension "$ext" > /dev/null 2>&1
+        fi
+    done
+fi
+
+if [[ $printed -eq 0 ]]; then
+    echo ""
+fi
