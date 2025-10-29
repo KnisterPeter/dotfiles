e613e417b3561eb7c54bab906429308e16af5651
Markus Wolf <mail@markus-wolf.de>
2025-10-29 18:53:02 +0100

    fix: golang install test

--- tools/09-go.sh
+++ tools/09-go.sh
@@ -3,7 +3,7 @@
 echo -n "  🔹 go"
 
 # shellcheck disable=SC2155
-declare -r go_pkg="$(dpkg --list 'golang-[.0-9]*-go' | grep -e '^ii' | cut -d " " -f 3)"
+declare -r go_pkg="$(dpkg --list 'golang-[.0-9]*-go' | grep -e '^ii' | cut -d " " -f 3 | head -n 1)"
 if [[ -n "${go_pkg}" ]] ; then
     PATH="$PATH:$(dpkg --listfiles "${go_pkg}" | grep -e "/bin$")"
 else
