47b9ff671672277700a8cf25df6c5afd577029fb
Markus Wolf <mail@markus-wolf.de>
2025-11-24 14:37:31 +0100

    fix: update flutter support

--- tools/06-flutter.sh
+++ tools/06-flutter.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-echo -n "  🔹 Flutter (lazy)"
+echo -n "  🔹 Flutter"
 
-setup-flutter() {
-    if which flutter > /dev/null ; then
-        # shellcheck disable=SC1090
-        . <(flutter bash-completion)
-    fi
-}
+if [ -d "$HOME/Downloads/flutter" ] ; then
+    export PATH="$HOME/Downloads/flutter/bin/:$PATH"
+fi
+
+if which flutter > /dev/null ; then
+    # shellcheck disable=SC1090
+    . <(flutter bash-completion)
+fi
 
 echo ""
