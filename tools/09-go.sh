9ea020b172a34400b164c1026e98955ba72243fb
Markus Wolf <mail@markus-wolf.de>
2026-04-01 09:07:47 +0200

    add golang paths

--- tools/09-go.sh
+++ tools/09-go.sh
@@ -6,6 +6,10 @@ echo -n "  🔹 go"
 declare -r go_pkg="$(dpkg --list 'golang-[.0-9]*-go' | grep -e '^ii' | cut -d " " -f 3 | head -n 1)"
 if [[ -n "${go_pkg}" ]] ; then
     PATH="$PATH:$(dpkg --listfiles "${go_pkg}" | grep -e "/bin$")"
+
+    if [[ -d "$HOME/go/bin" ]] ; then
+        PATH="$PATH:$HOME/go/bin"
+    fi
 else
     echo -n " - failed"
 fi
